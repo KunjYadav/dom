a2687dfdbfda7d8230c7e21de808ac68eec84f06
KunjYadav <kunj.yadav555@gmail.com>
2022-04-14 21:44:56 +0530

    Add a new li element

--- dom.js
+++ dom.js
@@ -27,15 +27,31 @@ var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000';
 
 // GETELEMENTSBYCLASSNAME //
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-console.log(items[0]);
-items[0].style.fontWeight = 'bold';
-console.log(items[1]);
-items[1].style.fontWeight = 'bold';
-console.log(items[2]);
-items[2].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
-console.log(items[3]);
-items[3].style.fontWeight = 'bold';
+// // Gives error
+// // items.style.backgroundColor = '#f4f4f4';
+
+// for(var i = 0; i < items.length; i++) {
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+
+// GETELEMENTSBYTAGNAME //
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+// Gives error
+// items.style.backgroundColor = '#f4f4f4';
+
+for(var i = 0; i < li.length; i++) {
+    li[i].style.backgroundColor = '#f4f4f4';
+}
